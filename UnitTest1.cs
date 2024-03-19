29bca57902472cb6159359b536f2225b8380cebe
Igor <Igor@DESKTOP-4KOTUNQ>
2024-03-19 09:29:13 +0100

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace practice
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
